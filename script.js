194594079763f22f03d535fda0d407e394684ab7
buggy_bot <67285671+pushkar7570@users.noreply.github.com>
2023-08-03 20:30:53 +0530

    button function integrate

--- script.js
+++ script.js
@@ -7,6 +7,17 @@
  *   Webflow.push(readyFunction);
  */
 
+function handleClick() {
+        console.log('Button clicked!')
+        window.botpressWebChat.sendEvent({ type: 'show' })
+      }
+ 
+      const button1 = document.getElementById('gs1')
+      button1.addEventListener('click', handleClick)
+
+      const button = document.getElementById('gs2')
+      button.addEventListener('click', handleClick)
+
 (() => {
     var __getOwnPropNames = Object.getOwnPropertyNames;
     var __commonJS = (cb, mod) => function __require() {
